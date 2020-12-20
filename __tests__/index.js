b87836d62a50618483a31a3a76850d3a89d1109c
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2020-12-20 19:25:19 +0100

    Adds some tests (which work as expected)

--- __tests__/index.js
+++ __tests__/index.js
@@ -6,6 +6,8 @@ import fs from 'fs';
 import mockAxios from 'jest-mock-axios';
 import audioLoader from '../src';
 
+const PIANO_JS_URL = '__assets__/piano.js';
+
 const MockAudioContext = {
   decodeAudioData: jest.fn((array, onSuccess, onError) => {
     if (array instanceof ArrayBuffer) onSuccess(array.slice(0));
@@ -19,10 +21,9 @@ afterEach(() => {
 });
 
 it('loads MIDI.js soundfont', async () => {
-  const URL = '__assets__/piano.js';
-  let soundfont = audioLoader(URL, { context: MockAudioContext });
-  mockAxios.mockResponseFor({ responseType: 'text', url: URL }, {
-    data: fs.readFileSync(`${__dirname}/${URL}`, 'utf8'),
+  let soundfont = audioLoader(PIANO_JS_URL, { context: MockAudioContext });
+  mockAxios.mockResponseFor({ responseType: 'text', url: PIANO_JS_URL }, {
+    data: fs.readFileSync(`${__dirname}/${PIANO_JS_URL}`, 'utf8'),
     status: 200,
   });
   soundfont = await soundfont;
@@ -35,8 +36,14 @@ it('loads MIDI.js soundfont', async () => {
 });
 
 it('refuses to work without AudioContext provided', () => {
-  expect(() => audioLoader('__assets__/piano.js'))
+  expect(() => audioLoader(PIANO_JS_URL))
     .toThrowErrorMatchingSnapshot();
-  expect(() => audioLoader('__assets__/piano.js', {}))
+  expect(() => audioLoader(PIANO_JS_URL, {}))
     .toThrowErrorMatchingSnapshot();
+});
+
+it('throws error on network issues', async () => {
+  const soundfont = audioLoader(PIANO_JS_URL, { context: MockAudioContext });
+  mockAxios.mockError(Error('Test network error'));
+  expect(soundfont).rejects.toThrowErrorMatchingSnapshot();
 });
